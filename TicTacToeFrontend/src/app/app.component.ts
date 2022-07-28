8a8cd454d22178cc49579ec232782623abbbe603
Jack <kolak.jacek@gmail.com>
2022-07-28 23:54:11 +0200

    testing stuff

--- TicTacToeFrontend/src/app/app.component.ts
+++ TicTacToeFrontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import * as SockJS from "sockjs-client";
 import * as Stomp from 'stompjs';
@@ -11,7 +11,8 @@ import * as Stomp from 'stompjs';
 export class AppComponent {
 
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   connectionUrl = 'http://localhost:8080/api/v1/';
   title = 'TicTacToeFrontend';
@@ -22,8 +23,8 @@ export class AppComponent {
   print() {
     console.log('gameid ' + this.gameId);
     console.log('controlka ' + this.hello);
-    console.log('topic name ' + this.topicPrefix + this.hello);
-    console.log('endpoint ' + this.prefix + '/' + this.hello);
+    // console.log('topic name ' + this.topicPrefix + this.hello);
+    console.log('endpoint ' + this.appPrefix + '/' + this.hello);
   }
 
   getFromServer() {
@@ -38,8 +39,10 @@ export class AppComponent {
   webSocketEndPoint: string = 'http://localhost:8080/game';
   hello: string = '';
   topicPrefix: string = "/topic/room/";
-  prefix: string = '/app/dupa';
+  appPrefix: string = '/app/dupa';
   stompClient: any;
+
+  canContinue: boolean;
 
   connect() {
     console.log("Initialize WebSocket Connection");
@@ -49,17 +52,32 @@ export class AppComponent {
 
     // _this.stompClient.disconnect() // TODO
 
+
+    // czyli tak -> subskrypcja tylko na chwilę, by ogarnąc czy można w ogóle
+    // jeśli tak to spoko, jeśli nei ro rezygnuje
+
+    // connect łączy się do sockJS
     _this.stompClient.connect({}, function () {
-      _this.stompClient.subscribe(_this.topicPrefix + _this.hello, function (sdkEvent: String) {
-      // _this.stompClient.subscribe(_this.prefix + '/' + _this.hello, function (sdkEvent: String) {
+
+      // ten jest do uzyskania czy można kontynować
+      _this.stompClient.subscribe(_this.appPrefix + '/' + _this.hello, function (sdkEvent: String) { // to jest próba
         _this.onMessageReceived(sdkEvent);
       });
+
+      // ten jest do kontynuowania
+      _this.canContinue ?
+      _this.stompClient.subscribe(_this.topicPrefix + _this.hello, function (sdkEvent: String) { // to działało
+        _this.onMessageReceived(sdkEvent);
+      }) : console.log('ni mozna');
+
       //_this.stompClient.reconnect_delay = 2000;
     }, this.errorCallBack);
   };
 
   onMessageReceived(message: String) {
     console.log("Message Recieved from Server :: " + message);
+
+    this.canContinue = message.toString().endsWith('1');
   }
 
   errorCallBack(error: String) {
@@ -70,15 +88,20 @@ export class AppComponent {
   }
 
   send() {
-    console.log("calling logout api via web socket");
-    this.stompClient.send(this.prefix + '/' + this.hello, {}, JSON.stringify({gameId: this.hello, fieldNo: '5', nominal: '1'}));
+    console.log("sending");
+    this.canContinue ?
+      this.stompClient.send(this.appPrefix + '/' + this.hello, {}, JSON.stringify({
+        gameId: this.hello,
+        fieldNo: '5',
+        nominal: '1'
+      }))
+      : console.log('nie mozna');
   }
 }
 
 class GameWithId {
   game: Game;
   gameId: String;
-
 
   constructor(game: Game, gameId: String) {
     this.game = game;
--- TicTacToeWebsocket/src/main/java/pl/kolak/tictactoewebsocket/Controller.java
+++ TicTacToeWebsocket/src/main/java/pl/kolak/tictactoewebsocket/Controller.java
@@ -1,21 +1,14 @@
 package pl.kolak.tictactoewebsocket;
 
 import core.Game;
-import org.springframework.context.event.EventListener;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
-import org.springframework.messaging.support.GenericMessage;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.socket.messaging.SessionConnectEvent;
-import org.springframework.web.socket.messaging.SessionConnectedEvent;
-import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -43,80 +36,75 @@ public class Controller {
     public GameWithId createGame() {
         String gameId = UUID.randomUUID().toString();
 
-        playersAtGame.putIfAbsent(gameId, 1);
-
         return new GameWithId(gameId, gameService.createGame(gameId));
     }
 
-    @EventListener
-    public void handleSessionSubscribeEvent(SessionSubscribeEvent event) {
-        System.out.println("SessionSubscribeEvent");
+//    @EventListener
+//    public void handleSessionSubscribeEvent(SessionSubscribeEvent event) {
+//        System.out.println("===SessionSubscribeEvent===");
+//
+//        GenericMessage message = (GenericMessage) event.getMessage();
+//        String simpDestination = (String) message.getHeaders().get("simpDestination");
+//        MessageHeaders headers = message.getHeaders();
+//        headers.forEach((s, o) -> System.out.println(s + " " + o.toString()));
+//
+//        if (simpDestination.startsWith("/topic/room/")) {
+//            System.out.println("o chuj");
+//        }
+//        System.out.println("===SessionSubscribeEvent===");
+//    }
+//
+//    @EventListener
+//    public void connect(SessionConnectEvent event) {
+//        System.out.println("===SessionConnectEvent===");
+//
+//        GenericMessage message = (GenericMessage) event.getMessage();
+//        String simpDestination = (String) message.getHeaders().get("simpDestination");
+//        MessageHeaders headers = message.getHeaders();
+//        headers.forEach((s, o) -> System.out.println(s + " " + o.toString()));
+//
+//        System.out.println("===SessionConnectEvent===");
+//    }
+//
+//    @EventListener
+//    public void connected(SessionConnectedEvent event) {
+//        System.out.println("===SessionConnectEvent===");
+//
+//        GenericMessage message = (GenericMessage) event.getMessage();
+//        String simpDestination = (String) message.getHeaders().get("simpDestination");
+//        MessageHeaders headers = message.getHeaders();
+//        headers.forEach((s, o) -> System.out.println(s + " " + o.toString()));
+//
+//        System.out.println("===SessionConnectEvent===");
+//    }
+//
+//    @EventListener
+//    public void another(SessionDisconnectEvent event) {
+//        System.out.println("===SessionDisconnectEvent===");
+//
+//        GenericMessage message = (GenericMessage) event.getMessage();
+//        String simpDestination = (String) message.getHeaders().get("simpDestination");
+//        MessageHeaders headers = message.getHeaders();
+//        headers.forEach((s, o) -> System.out.println(s + " " + o.toString()));
+//
+//        System.out.println("===SessionDisconnectEvent===");
+//    }
 
-        GenericMessage message = (GenericMessage) event.getMessage();
-        String simpDestination = (String) message.getHeaders().get("simpDestination");
-        MessageHeaders headers = message.getHeaders();
-        headers.forEach((s, o) -> System.out.println(s + " " + o.toString()));
-
-        if (simpDestination.startsWith("/topic/room/")) {
-            System.out.println("o chuj");
-        }
-    }
-
-    @EventListener
-    public String connect(SessionConnectEvent event) {
-        System.out.println("SessionConnectEvent");
-
-        GenericMessage message = (GenericMessage) event.getMessage();
-        String simpDestination = (String) message.getHeaders().get("simpDestination");
-        MessageHeaders headers = message.getHeaders();
-        headers.forEach((s, o) -> System.out.println(s + " " + o.toString()));
-
-        return "HEEEEEELo";
-    }
-
-    @EventListener
-    public String connected(SessionConnectedEvent event) {
-        System.out.println("SessionConnectEvent");
-
-        GenericMessage message = (GenericMessage) event.getMessage();
-        String simpDestination = (String) message.getHeaders().get("simpDestination");
-        MessageHeaders headers = message.getHeaders();
-        headers.forEach((s, o) -> System.out.println(s + " " + o.toString()));
-
-        return "HALO";
-    }
-
-    @EventListener
-    public void another(SessionDisconnectEvent event) {
-        System.out.println("SessionDisconnectEvent");
-
-        GenericMessage message = (GenericMessage) event.getMessage();
-        String simpDestination = (String) message.getHeaders().get("simpDestination");
-        MessageHeaders headers = message.getHeaders();
-        headers.forEach((s, o) -> System.out.println(s + " " + o.toString()));
-
-        throw new IllegalArgumentException();
-    }
-
+    // send()
     @MessageMapping("/dupa/{gameId}")
     @SendTo("/topic/room/{gameId}")
-//    @SendTo("/app/dupa/{gameId}")
     public Game method(@DestinationVariable String gameId, GameData gameData) {
-        System.out.println(gameId);
-        System.out.println(gameData);
-
-        playersAtGame.put(gameId, playersAtGame.get(gameId) + 1);
 
         return gameService.updateGame(gameData.gameId, Integer.parseInt(gameData.fieldNo), Integer.parseInt(gameData.nominal));
     }
 
-    @SubscribeMapping("/dupa")
-    @SendTo("/topic/room/{gameId}")
+    @SubscribeMapping("/dupa/{gameId}")
     public String method(@DestinationVariable String gameId) {
         System.out.println(gameId);
         System.out.println("jest kurwa!!!");
 
-        return "ja pierdolle";
+        return gameId.equals("s") ? "1" : "0";
+
     }
 
     record GameWithId(String gameId, Game game) { }
