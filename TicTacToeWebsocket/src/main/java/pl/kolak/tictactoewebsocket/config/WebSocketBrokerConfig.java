58ab2fa9f365090cd44b01d0f636ed15d8b9506c
Jack <kolak.jacek@gmail.com>
2023-10-21 09:27:04 +0200

    update

--- TicTacToeWebsocket/src/main/java/pl/kolak/tictactoewebsocket/config/WebSocketBrokerConfig.java
+++ TicTacToeWebsocket/src/main/java/pl/kolak/tictactoewebsocket/config/WebSocketBrokerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.lang.NonNull;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import pl.kolak.tictactoewebsocket.util.Constants;
 
@@ -15,7 +16,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(@NonNull MessageBrokerRegistry registry) {
         WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
-
         registry.enableSimpleBroker("");
         registry.setApplicationDestinationPrefixes("");
     }
@@ -27,5 +27,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .addEndpoint("/game")
                 .setAllowedOrigins(Constants.CORS_URL)
                 .withSockJS();
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new MyWebSocketHandler(), "/my-websocket-endpoint");
     }
 }
